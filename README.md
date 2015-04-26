0e4438503ab8ebf3349a957b4f9ef299d1b6cf94
kumak1 <kumaki0@gmail.com>
2015-04-26 17:30:34 +0900

    雛形を作る

---
+++ README.md
@@ -0,0 +1,10 @@
+# web-gl-plactice
+WebGL 勉強会の実践編
+
+## startup guide
+
+```sh
+npm install -g gulp
+npm install
+gulp build
+```
---
+++ gulp/config.js
@@ -0,0 +1,61 @@
+var path = require('path');
+
+var dest = './build';
+var src = './src';
+var relativeSrcPath = path.relative('.', src);
+
+module.exports = {
+
+  dest: dest,
+
+  js: {
+    src: src + '/js/**',
+    dest: dest + '/js',
+    uglify: false
+  },
+
+  webpack: {
+    entry: src + '/js/app.js',
+    output: {
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['', '.js']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader' // <- without es6 polyfills
+          // loader: 'babel-loader?experimental&optional=selfContained'
+        }
+      ]
+    }
+  },
+
+  copy: {
+    src: [
+      src + '/www/index.html'
+    ],
+    dest: dest
+  },
+
+  stylus: {
+    src: [
+      src + '/styl/**/!(_)*'
+    ],
+    dest: dest + '/css/',
+    output: 'app.css',
+    autoprefixer: {
+      browsers: ['last 2 versions']
+    },
+    minify: false
+  },
+
+  watch: {
+    js: relativeSrcPath + '/js/**',
+    styl: relativeSrcPath + '/styl/**',
+    www: relativeSrcPath + '/www/index.html'
+  }
+};
